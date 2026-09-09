1e91da989fa316fa01c46faaa7a9932b50d9ae2a
Tshepang298 <ST10498742@RCCONNECT.EDU.ZA>
2026-09-09 09:45:12 +0200

    Update CybersecurityChatbot.AssemblyInfo.cs

--- CybersecurityChatbot/CybersecurityChatbot/obj/Debug/net9.0/CybersecurityChatbot.AssemblyInfo.cs
+++ CybersecurityChatbot/CybersecurityChatbot/obj/Debug/net9.0/CybersecurityChatbot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d7fcfa36b80f98a81610854ff2f02808be5241f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50319416091d13fe1efe5748ce53c556c3fb3db6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CybersecurityChatbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
